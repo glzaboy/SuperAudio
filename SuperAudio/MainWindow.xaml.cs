31a79fb7e6d28890c920759c5a2a1fe829a530ed
glzaboy <glzaboy@163.com>
2026-06-06 22:08:12 +0800

    增加左侧菜单布局

--- SuperAudio/MainWindow.xaml.cs
+++ SuperAudio/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using SuperAudio.Helps;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Threading.Tasks;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,11 +15,70 @@ namespace SuperAudio
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        public Action? NavigationViewLoaded { get; set; }
         public MainWindow()
         {
             InitializeComponent();
             this.ExtendsContentIntoTitleBar = true;
-            SetTitleBar(AppTitleBar);
+        }
+        private void RootGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            // We need to set the minimum size here because the XamlRoot is not available in the constructor.
+
+        }
+        private void TitleBar_PaneToggleRequested(TitleBar sender, object args)
+        {
+            NavigationViewControl.IsPaneOpen = !NavigationViewControl.IsPaneOpen;
+        }
+        private void OnRootFrameNavigated(object sender, NavigationEventArgs e)
+        {
+            //TestContentLoadedCheckBox.IsChecked = true;
+        }
+
+        private void OnRootFrameNavigating(object sender, NavigatingCancelEventArgs e)
+        {
+            //TestContentLoadedCheckBox.IsChecked = false;
+        }
+        private void OnPaneDisplayModeChanged(NavigationView sender, NavigationViewDisplayModeChangedEventArgs args)
+        {
+            if (sender.PaneDisplayMode == NavigationViewPaneDisplayMode.Top)
+            {
+                titleBar.IsPaneToggleButtonVisible = false;
+            }
+            else
+            {
+                titleBar.IsPaneToggleButtonVisible = true;
+            }
+        }
+        private void OnNavigationViewControlLoaded(object sender, RoutedEventArgs e)
+        {
+            // Delay necessary to ensure NavigationView visual state can match navigation
+            Task.Delay(500).ContinueWith(_ => this.NavigationViewLoaded?.Invoke(), TaskScheduler.FromCurrentSynchronizationContext());
+
+            var navigationView = sender as NavigationView;
+            navigationView?.RegisterPropertyChangedCallback(NavigationView.IsPaneOpenProperty, OnIsPaneOpenChanged);
+        }
+        private void OnIsPaneOpenChanged(DependencyObject sender, DependencyProperty dp)
+        {
+            if (sender is not NavigationView navigationView)
+            {
+                return;
+            }
+
+            var announcementText = navigationView.IsPaneOpen ? "Navigation Pane Opened" : "Navigation Pane Closed";
+
+            UIHelper.AnnounceActionForAccessibility(navigationView, announcementText, "NavigationViewPaneIsOpenChangeNotificationId");
+        }
+        private void OnNavigationViewSelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+
+        }
+        private void TitleBar_BackRequested(TitleBar sender, object args)
+        {
+            if (this.rootFrame.CanGoBack)
+            {
+                this.rootFrame.GoBack();
+            }
         }
     }
 }
